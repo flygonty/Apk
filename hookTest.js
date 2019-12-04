72c69468b421b8ca717fed3f845cb7b1dc4d7b84
flygonty <willy.liu4610@gmail.com>
2019-12-05 01:20:15 +0800

    Update hookTest.js

--- hookTest.js
+++ hookTest.js
@@ -1,6 +1,7 @@
 Java.perform( function () {
     send("Hook Start...");
     // Feature Hook
+    
     Java.use("android.telephony.TelephonyManager").getImei.overload().implementation=function() {
         return "353436042253624" ;
     } 
@@ -15,12 +16,21 @@ Java.perform( function () {
     } 
     // Build & System properties Hook
     var ver = Java.use('android.os.Build') ;
-    ver.HARDWARE.value = "Samsung S10" ;
-    console.log("Hardware : " + ver.HARDWARE.value);
-    // ver.HARDWARE = "Sama;sldfj" ;
-    // ver.Hardware = "Samsung S10" ;
-    // console.log("Hardware after : " + ver.Hardware) ;
-    
+    ver.SERIAL.value = "ABC756867561766" ;
+    ver.HARDWARE.value = "mdfpp" ;
+    ver.BRAND.value = "Samsung" ;
+    ver.MODEL.value = "Samsung S8" ;
+    ver.TAGS.value = "keys" ;
+    // console.log("Hardware : " + ver.BRAND.value);
+
+    // Battery Hook
+    var Battery = Java.use('android.os.BatteryManager') ;
+    Battery.EXTRA_LEVEL.value = "95" ;
+    Battery.EXTRA_SCALE.value = "100" ;
+    Battery.BATTERY_PROPERTY_CAPACITY.value = 50 ;
+    // console.log(Battery.BATTERY_PROPERTY_CAPACITY.value) ;
+
+
     // Wifi Hook
     Java.use("android.net.wifi.WifiInfo").getMacAddress.overload().implementation=function() {
         return "7C-57-23-06-C4-11" ;
